2207705e5714c57a2f5fe60faae0ef89fd1f71fa
mohourzik <our.mohand@gmail.com>
2024-09-16 06:57:33 +0000

    Add the class DeepQNetwork.

---
+++ network.py
@@ -0,0 +1,18 @@
+import torch.nn as nn
+
+
+class DeepQNetwork(nn.Module):
+    def __init__(self, n_inputs, n_actions, h1 = 256, h2 = 512):
+        super().__init__()
+        self.Q = nn.Sequential(
+            nn.Linear(n_inputs, h1),
+            nn.ReLU(),
+            nn.Linear(h1, h2),
+            nn.ReLU(),
+            nn.Linear(h2, n_actions)
+        )
+
+    def forward(self, state):
+        actions = self.Q(state)
+
+        return actions
